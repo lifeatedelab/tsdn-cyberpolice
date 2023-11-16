52b38a548db946df819014781630fc82b7e0f593
ingeniom22 <jamesmichael0444@gmail.com>
2023-11-16 09:16:44 +0700

    working dashboard

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
+import json
 from fastapi import FastAPI, Request, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from nfstream import NFStreamer
-import joblib
+import joblib   
+import plotly
 from xgboost import XGBClassifier
 import pandas as pd
 import uvicorn
 from starlette.middleware.trustedhost import TrustedHostMiddleware
+import plotly.express as px
 
 app = FastAPI(
     title="Cyberpolice",
@@ -43,8 +46,8 @@ def get_df_from_pcap(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=f"Error during conversion: {str(e)}")
 
 # Function to analyze pcap and make predictions
-def analyze_pcap(file: UploadFile = File(...)):
-    df = get_df_from_pcap(file)
+def analyze_df(df: pd.DataFrame):
+    # df = get_df_from_pcap(file)
 
     features = [
         'bidirectional_first_seen_ms',
@@ -63,6 +66,35 @@ def analyze_pcap(file: UploadFile = File(...)):
 
     return {"predictions": predictions.tolist()}
 
+def get_class_pie(predictions):
+    count_class0 = predictions.count(0)
+    count_class1 = predictions.count(1)
+
+    labels = ['Class 0', 'Class 1']
+    values = [count_class0, count_class1]
+
+    fig = px.pie(names=labels, values=values, hole=0.3, title='Class Distribution Pie Chart', template="plotly_dark")
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+def get_bfs_histogram(df):
+    # df = px.data.tips()
+    fig = px.histogram(df, x="bidirectional_first_seen_ms", template="plotly_dark")
+    # print(df["dst2src_fin_packets"].unique())
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+def get_application_bar(df):
+    # df = px.data.tips()
+    fig = px.histogram(df, y="application_name", template="plotly_dark").update_yaxes(categoryorder='total ascending')
+    # print(df["dst2src_fin_packets"].unique())
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+    
+
 # Route for testing if the app is running
 @app.get("/")
 def index(request: Request):
@@ -71,8 +103,18 @@ def index(request: Request):
 # Route to analyze pcap and make predictions
 @app.post("/analyze")
 async def analyze_endpoint(request: Request, file: UploadFile = File(...)):
-    result = analyze_pcap(file=file)
-    return templates.TemplateResponse("dashboard.html", {"request" : request})
+    df = get_df_from_pcap(file=file)
+    results = analyze_df(df)['predictions'] #a binary array
+    pie_chart = get_class_pie(results)
+    bfs_histogram = get_bfs_histogram(df=df)
+    app_bar = get_application_bar(df=df)
+
+    return templates.TemplateResponse("dashboard.html", 
+                                      {
+                                          "request" : request,
+                                            "pie_chart": pie_chart,
+                                              "bfs_histogram" : bfs_histogram,
+                                              "app_bar": app_bar})
 
 # Run the app using UVicorn
 if __name__ == "__main__":
